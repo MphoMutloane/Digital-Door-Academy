86363ec72f97e732d0ac211294718e9bf6b157eb
mpho <mphomutloane.mm@gmail.com>
2026-03-14 17:54:18 +0200

    Added clerk signup and signin

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,72 +1,121 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import { navbarStyles } from '../assets/dummyStyles'
 import logo from '../assets/logo.png'
-import {BookMarked, BookOpen, Contact, Home, Users} from 'lucide-react';
-import { NavLink } from 'react-router-dom';
+import { BookMarked, BookOpen, Contact, Home, Menu, Users, X } from 'lucide-react'
+import { NavLink } from 'react-router-dom'
+import { useClerk, useUser, useAuth, UserButton } from '@clerk/react'
 
 const navItems = [
-  { name: "Home", icon: Home, href: "/" },
-  { name: "Courses", icon: BookOpen, href: "/courses" },
-  { name: "About", icon: BookMarked, href: "/about" },
-  { name: "Faculty", icon: Users, href: "/faculty" },
-  { name: "Contact", icon: Contact, href: "/contact" },
+    { name: "Home", icon: Home, href: "/" },
+    { name: "Courses", icon: BookOpen, href: "/courses" },
+    { name: "About", icon: BookMarked, href: "/about" },
+    { name: "Faculty", icon: Users, href: "/faculty" },
+    { name: "Contact", icon: Contact, href: "/contact" },
 ];
 
 const Navbar = () => {
 
+    // for clerk
+    const { openSignUp } = useClerk();
+    const { isSignedIn } = useUser();
+    const { getToken } = useAuth();
+
+    // for mobile toggle
+    const [isOpen, setIsOpen] = useState(false);
+    const [lastScrollY, setLastScrollY] = useState(0);
+
     const [isScrolled, setIsScrolled] = useState(false);
     const [showNavbar, setShowNavbar] = useState(true);
 
+    const menuRef = useRef(null);
+    const isLoggedIn = isSignedIn && Boolean(localStorage.getItem('token'));
+
     const desktopLinkClass = (isActive) =>
-        `${navbarStyles.desktopNavItem} ${
-            isActive ? navbarStyles.desktopNavItemActive : ""
+        `${navbarStyles.desktopNavItem} ${isActive ? navbarStyles.desktopNavItemActive : ""
         }`;
 
     const mobileLinkClass = (isActive) =>
-        `${navbarStyles.mobileMenuItem} ${
-            isActive ? navbarStyles.mobileMenuItemActive : navbarStyles.mobileMenuItemHover
+        `${navbarStyles.mobileMenuItem} ${isActive ? navbarStyles.mobileMenuItemActive : navbarStyles.mobileMenuItemHover
         }`;
 
 
-  return (
-    <nav className={`${navbarStyles.navbar} ${
-        showNavbar ? navbarStyles.navbarVisible : navbarStyles.navbarHidden
-    } ${
-        isScrolled ? navbarStyles.navbarScrolled : navbarStyles.navbarDefault
-    }`}>
-        <div className={navbarStyles.container}>
-            <div className={navbarStyles.innerContainer}>
-                {/* LOGO */}
-                <div className='flex items-center gap-3 select-none'>
-                    <img src={logo} alt="Logo" className='w-15 h-15' />
-                </div>
-                {/* Desktop Navigation */}
-                <div className={navbarStyles.desktopNav}>
-                    <div className={navbarStyles.desktopNavContainer}>
-                        {navItems.map((item) => {
-                            const Icon = item.icon;
-                            return(
-                                <NavLink key={item.name} to={item.href} end={item.href === '/'} className={({isActive}) => desktopLinkClass(isActive)}>
-                                    <div className=' flex items-center space-x-2'>
-                                        <Icon size={16} className={navbarStyles.desktopNavIcon} />
-                                        <span className={navbarStyles.desktopNavText}>
-                                            {item.name}
-                                        </span>
-                                    </div>
-                                </NavLink>
-                            )
-                        })}
+    return (
+        <nav className={`${navbarStyles.navbar} ${showNavbar ? navbarStyles.navbarVisible : navbarStyles.navbarHidden
+            } ${isScrolled ? navbarStyles.navbarScrolled : navbarStyles.navbarDefault
+            }`}>
+            <div className={navbarStyles.container}>
+                <div className={navbarStyles.innerContainer}>
+                    {/* LOGO */}
+                    <div className='flex items-center gap-3 select-none'>
+                        <img src={logo} alt="Logo" className='w-15 h-15' />
+                    </div>
+                    {/* Desktop Navigation */}
+                    <div className={navbarStyles.desktopNav}>
+                        <div className={navbarStyles.desktopNavContainer}>
+                            {navItems.map((item) => {
+                                const Icon = item.icon;
+                                return (
+                                    <NavLink key={item.name} to={item.href} end={item.href === '/'} className={({ isActive }) => desktopLinkClass(isActive)}>
+                                        <div className=' flex items-center space-x-2'>
+                                            <Icon size={16} className={navbarStyles.desktopNavIcon} />
+                                            <span className={navbarStyles.desktopNavText}>
+                                                {item.name}
+                                            </span>
+                                        </div>
+                                    </NavLink>
+                                )
+                            })}
+                        </div>
+                    </div>
+
+                    {/* Right Side */}
+                    <div className={navbarStyles.authContainer}>
+                        {!isSignedIn ? (
+                            <button type='button' onClick={() => openSignUp({})} className={
+                                navbarStyles.createAccountButton ?? navbarStyles.loginButton
+                            }>
+                                <span>Create Account</span>
+                            </button>
+                        ) : (
+                            <div className=' flex items-center'>
+                                <UserButton afterSignOutUrl='/' />
+                            </div>
+                        )}
+
+                        {/* toggle */}
+                        <button onClick={() => setIsOpen(!isOpen)} className={navbarStyles.mobileMenuButton} >
+                            {isOpen ? <X size={20} /> : <Menu size={20} />}
+                        </button>
                     </div>
                 </div>
+                {/* mobile nav */}
+                <div ref={menuRef} className={`${navbarStyles.mobileMenu} ${isOpen ? navbarStyles.mobileMenuOpen : navbarStyles.mobileMenuClosed
+                    }`}>
+                    <div className={navbarStyles.mobileMenuContainer}>
+                        <div className={navbarStyles.mobileMenuItems}>
+                            {navItems.map((item) => {
+                                const Icon = item.icon;
+                                return (
+                                    <NavLink 
+                                    key={item.name} 
+                                    to={item.href} 
+                                    end={item.href === '/'} 
+                                    className={({ isActive }) => mobileLinkClass(isActive)}
+                                    onClick={() => setIsOpen(false)} 
+                                    >
+                                        <div className={navbarStyles.mobileMenuIconContainer}>
+                                            <Icon size={18} className={navbarStyles.mobileMenuIcon} />
+                                        </div>
 
-                {/* Right Side */}
-                <div>
-                    
+                                    </NavLink>
+                                )
+                            })}
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>
-    </nav>
-  )
+        </nav>
+    )
 }
 
 export default Navbar
